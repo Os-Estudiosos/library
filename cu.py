41438d7bbe948b8fde16a84422bedb435bd22989
Alex Oliveira <iamsralex@gmail.com>
2025-06-09 20:48:46 -0300

    Deletei aqruivo de teste

--- cu.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-cu = [(1,2, "Alex"), (3,4, "Bob"), (5,6, "Charlie")]
-df = pd.DataFrame(cu, columns=["x", "y", "name"])
-df["junto"] = df["x"].astype(str) + df["y"].astype(str)
-print(df)
