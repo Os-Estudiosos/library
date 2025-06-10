a78074071669aad5749ce8b947849365cae07861
João Pedro <joao.profissional.45@gmail.com>
2025-06-10 10:28:12 -0300

    Livros e categorias associados

--- gui/screens/categorias.py
+++ gui/screens/categorias.py
@@ -90,7 +90,7 @@ class Categories(Screen):
 
         pagination = TablesManager.categoriaTable.read(qtd=self.items_per_page, pagina=page)
 
-        table = Table(self.app, "edit_categories", TablesManager.categoriaTable, "idcat")
+        table = Table(self.app, "edit_categories", TablesManager.categoriaTable, "idcat", "see_category")
         table.build(pagination)
 
 
@@ -259,3 +259,71 @@ class CreateCategory(Screen):
             "idcat": current_idres+1,
         },values)
         RouteManager.go_back()
+
+
+class SeeCategory(Screen):
+    def __init__(self, app):
+        self.app = app
+        self.items_per_page = 10
+
+    def build(self, **kwargs):
+        category = kwargs["entry"]
+
+        title_frame = ctk.CTkFrame(
+            self.app,
+            fg_color="transparent"
+        )
+        title_frame.rowconfigure(0, weight=1)
+        title_frame.rowconfigure(1, weight=1)
+        title_frame.columnconfigure(0, weight=1)
+        title_frame.columnconfigure(1, weight=1)
+
+        title = ctk.CTkLabel(
+            title_frame,
+            text=f"{category.nomecat}",
+            font=("Arial", 20, "bold"),
+            text_color=Colors.SLATE.c_900,
+            justify="left"
+        )
+        title.grid(column=0, row=0, sticky="w")
+
+        description = ctk.CTkLabel(
+            title_frame,
+            text=f"Livros associados à categoria {category.nomecat}",
+            font=("Arial", 15, "bold"),
+            text_color=Colors.SLATE.c_900,
+            justify="left"
+        )
+        description.grid(column=0, row=1, sticky="w")
+
+        cancel_button = ctk.CTkButton(
+            title_frame,
+            command=lambda: RouteManager.go_to("classes"),
+            fg_color="#ffffff",
+            hover_color=Colors.GRAY.c_100,
+            text_color=Colors.GRAY.c_800,
+            border_color=Colors.GRAY.c_300,
+            border_width=1,
+            text="Voltar",
+            font=("Arial", 13, "bold"),
+            width=100,
+            corner_radius=10
+        )
+        cancel_button.grid(column=1, row=0, ipady=5, rowspan=2, sticky="e")
+
+        title_frame.grid(row=0, column=0, pady=10, padx=20, sticky="ew")
+
+        self.app.grid_rowconfigure(1, weight=1)
+        self.app.grid_columnconfigure(0, weight=1)
+
+        if "page" in kwargs.keys():
+            page = kwargs["page"]
+        else:
+            page = 1
+
+        pagination = TablesManager.lipertencecatTable.read(filter={
+            "idcat": int(category.idcat)
+        }, qtd=self.items_per_page, pagina=page)
+
+        table = Table(self.app, "edit_books", TablesManager.lipertencecatTable, ("isbnliv", "idcat"))
+        table.build(pagination)
